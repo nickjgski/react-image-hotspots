789f28a0d757203f3e997f4b163b6f871dce96d1
Nicolas Gutkowski <ngutkowski@riggsward.com>
2023-04-26 15:24:39 -0400

    Added build folder

---
+++ dist/Hotspot.d.ts
@@ -0,0 +1,17 @@
+export default Hotspot;
+declare class Hotspot extends React.Component<any, any, any> {
+    constructor(props: any);
+    constructor(props: any, context: any);
+    render(): JSX.Element;
+}
+declare namespace Hotspot {
+    namespace propTypes {
+        const x: PropTypes.Requireable<number>;
+        const y: PropTypes.Requireable<number>;
+        const content: PropTypes.Requireable<PropTypes.ReactElementLike>;
+        const style: PropTypes.Requireable<object>;
+    }
+}
+import React from 'react';
+import PropTypes from 'prop-types';
+//# sourceMappingURL=Hotspot.d.ts.map
